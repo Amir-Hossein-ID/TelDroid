0dbb1f5e156759e32c8b33f003d417b94107bf56
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2022-07-19 13:06:38 +0430

    update plugin manager

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,25 +1,33 @@
 import importlib
 import os
+import sys
 import traceback
 
 import shared
 
 class PluginManager:
     def __init__(self):
-        self.all_plugins = []
+        self.all_plugins = {}
         self.loaded_plugins = {}
-
-    def load_plugins(self):
+        self._find_plugins()
+    
+    def _find_plugins(self):
         for plugin in os.listdir(os.path.dirname(__file__)):
             if not plugin.endswith(".py") or plugin == "__init__.py":
                 continue
             plugin = plugin[:-3]
+            self.all_plugins[plugin] = None
+    
+    def _load_plugin(self, plugin):
+        importlib.import_module("plugins." + plugin)
+        self.all_plugins[plugin] = sys.modules["plugins." + plugin]
+
+    def load_plugins(self):
+        for plugin in self.all_plugins:
             try:
-                self.all_plugins.append(plugin)
                 self.loaded_plugins[plugin] = []
-                importlib.import_module("plugins." + plugin)
+                self._load_plugin(plugin)
             except Exception as e:
-                print(e)
                 traceback.print_exc()
                 del self.loaded_plugins[plugin]
     
